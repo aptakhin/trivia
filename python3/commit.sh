621beb0c0e2d8037d17d70a6d00be4e2be0c37cd
Alex Ptakhin <me@aptakhin.name>
2025-12-30 21:12:51 +0100

    [misc] Run pytest in commit

--- python3/commit.sh
+++ python3/commit.sh
@@ -5,11 +5,16 @@ then
   exit 1
 fi
 
+set -e
+
 echo "Staged changes"
 # git show staged file names only with modifications marked M, A or D and print modification
 git diff --cached --name-status | grep -E '^[MAD]' | while read status file; do
   echo "$status $file"
 done
+
+# uv tool run ruff check --fix .
+uv run pytest -s
 
 # read the message interactivelyfrom typed input
 echo "Enter commit message: "
