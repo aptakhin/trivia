a1bbe6aa6b533972b4d3e638d352597e46f776d0
Alex Ptakhin <me@aptakhin.name>
2025-12-30 20:55:11 +0100

    [primitive-obsession] Added commit script

---
+++ python3/commit.sh
@@ -0,0 +1,19 @@
+
+if [ -z "$1" ]
+then
+  echo "Usage: ./commit.sh <commit-prefix>"
+  exit 1
+fi
+
+echo "Staged changes"
+# git show staged file names only with modifications marked M, A or D and print modification
+git diff --cached --name-status | grep -E '^[MAD]' | while read status file; do
+  echo "$status $file"
+done
+
+# read the message interactivelyfrom typed input
+echo "Enter commit message: "
+read MESSAGE
+
+# commit staged files with message
+git commit -m "[$1] $MESSAGE"
